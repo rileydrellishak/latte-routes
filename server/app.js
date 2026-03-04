0908648b11deab86fa33a9f36d0dc0767a007b06
Riley Drellishak <rileydrellishak@gmail.com>
2026-03-04 15:58:24 -0600

    implements cors for front end to talk to back end

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import routesRouter from './routes/routes.routes.js';
+import cors from 'cors'
 
 const app = express();
 app.use(express.json())
+
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}
+app.use(cors(corsOptions))
 app.use('/routes', routesRouter);
 
 export default app;
