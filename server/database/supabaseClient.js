54dba108c44eff4a020cf2d00e3cd760d9783476
Riley Drellishak <rileydrellishak@gmail.com>
2026-03-10 17:31:13 -0500

    when i dropped the tables it ruined my life LOL

--- server/database/supabaseClient.js
+++ server/database/supabaseClient.js
@@ -4,11 +4,19 @@ import { createClient } from '@supabase/supabase-js';
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!supabaseUrl) throw new Error('SUPABASE_URL is missing from .env!');
 if (!supabaseKey) throw new Error('SUPABASE_SERVICE_KEY is missing from .env!');
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+// async function test() {
+//   const { data, error } = await supabase.from('coffee_shops').select('*').limit(1);
+//   console.log('Error:', error);
+//   console.log('Data:', data);
+// }
+
+// test();
 
 export default supabase;
